dc8bc779c5483bebf1549aeae2952f36dd303235
bahadir <lifepro69@gmail.com>
2023-12-25 03:19:09 +0300

    1

--- app.js
+++ app.js
@@ -1087,6 +1087,6 @@ app.post('/mesajdelete/:id', async (req, res) => {
 
 
 
-app.listen(port, () => {
-  console.log(`Uygulama ${port} portunda çalışıyor`);
-});
+//app.listen(port, () => {
+  //console.log(`Uygulama ${port} portunda çalışıyor`);
+//});
