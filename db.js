e1786c52ab41c1f25187d31caee7513c53b1a779
18bahadir06 <94183692+18bahadir06@users.noreply.github.com>
2023-12-25 16:03:44 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,14 +3,13 @@ import mongoose from "mongoose";
 const conn = () => {
     mongoose.connect(process.env.DB_URI, {
         dbName: "bahadirakkasdb",
-        useNewUrlParser: true,
-        // useUnifiedTopology seçeneğini kaldırın
+        useUnifiedTopology: true,
     })
     .then(() => {
-        console.log("Connected to the DB successfully");
+        console.log("Connect to the DB successfully");
     })
     .catch((err) => {
-        console.log('DB connection err:,' + err);
+        console.log('DB connection error:', err);
     });
 };
 
